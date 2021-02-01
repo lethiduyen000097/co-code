697dcfe45de981ffda07be53eda4aedc4590843a
lethiduyen000097 <duyenlt060097@gmail.com>
2021-02-01 11:59:35 +0700

    Page

--- code-app/src/App.js
+++ code-app/src/App.js
@@ -4,8 +4,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Xin chào</p>
+        <p>Dịch vụ vườn kính chào quý vị !</p>
       </header>
+      <nav className="App-menu">
+        <a href="#">Thăm quan</a> &nbsp; 
+        <a href="#">Chăm sóc vườn</a> &nbsp; 
+        <a href="#">Thu hoạch</a> &nbsp; 
+      </nav>
+      <main className="App-main">
+        <p>Đây là nội dung trang</p>
+      </main>
+      <footer className="App-footer">
+        <small>Hỗ trợ kỹ thuật: 0375893542 </small>
+      </footer>
     </div>
   );
 }
